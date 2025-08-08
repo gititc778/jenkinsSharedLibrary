b02d71b36126c01795a21054b62930e398343e92
Danish Ahmed <Danish.Ahmed@informationtechconsultants.com>
2025-08-08 10:56:24 +0500

    def in front of buildTag

--- vars/generateTag.groovy
+++ vars/generateTag.groovy
@@ -1,7 +1,9 @@
 def call() {
     def date = new Date().format('yyyyMMdd')
-    buildTag = "${date}.${env.BUILD_NUMBER}"
+    def buildTag = "${date}.${env.BUILD_NUMBER}"
     currentBuild.displayName = buildTag
     sh "echo BUILD_TAG=${buildTag} > build.env"
     return buildTag
 }
+
+
