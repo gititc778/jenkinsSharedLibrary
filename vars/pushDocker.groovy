8fc5ecef305931b6548a512dfdba94452c5e6f1c
gititc778 <Danish.Ahmed@informationtechconsultants.com>
2026-04-15 08:23:29 +0100

    Update pushDocker.groovy

--- vars/pushDocker.groovy
+++ vars/pushDocker.groovy
@@ -1,5 +1,5 @@
 def call(buildTag) {
-    withCredentials([usernamePassword(credentialsId: 'dockeritc778', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-login-itc', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             echo "${DOCKER_PASS}" | docker login -u "${DOCKER_USER}" --password-stdin
             docker tag sampleapp:${buildTag} ${DOCKER_USER}/sampleapp:${buildTag}
